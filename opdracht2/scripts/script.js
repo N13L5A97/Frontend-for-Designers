80595f91d63d9fb76aec4b0114fc912ec4a49585
N13L5A97 <niels.aling@hotmail.com>
2023-03-15 15:31:36 +0100

    idk

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,10 @@
-// JavaScript Document
-console.log("howdy");
+Request Url: http://gateway.marvel.com/v1/public/comics 
+Request Method: GET 
+Params: {   
+    "apikey": "44d6b73c0df2264d2a74201719831cee",   
+    "ts": "a timestamp",   
+    "hash": "your hash" 
+} 
+Headers: {   
+    Accept: */* 
+}
